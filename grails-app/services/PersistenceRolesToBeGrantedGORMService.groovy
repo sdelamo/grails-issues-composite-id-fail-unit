69809fbd2625390a86ecd359409860a04fa5c73e
Sergio del Amo <sergio.delamo@shoptimix.com>
2016-09-19 17:08:57 +0200

    Remove unnecessary service methods

--- grails-app/services/PersistenceRolesToBeGrantedGORMService.groovy
+++ grails-app/services/PersistenceRolesToBeGrantedGORMService.groovy
@@ -3,19 +3,6 @@ import grails.transaction.Transactional
 @Transactional
 class PersistenceRolesToBeGrantedGORMService {
 
-    def findAll() {
-        PersistenceRolesToBeGranted.findAll()
-    }
-
-    void save(String authority, String username) {
-
-        def p = new PersistenceRolesToBeGranted(username: username, authority: authority)
-
-        if ( !p.save() ) {
-            println p.errors
-        }
-    }
-
     List<String> findAllAuthorityByEmail(String email) {
         def c = PersistenceRolesToBeGranted.createCriteria()
         c.list {
@@ -24,14 +11,5 @@ class PersistenceRolesToBeGrantedGORMService {
             }
             eq('username', email)
         } as List<String>
-    }
-
-    void deleteRolesToByGrantedByEmail(String email) {
-        PersistenceRolesToBeGranted.executeUpdate('DELETE FROM PersistenceRolesToBeGranted WHERE username = :email', [email: email])
-    }
-
-    void remove(String roleName, String email) {
-        def p = PersistenceRolesToBeGranted.findByUsernameAndAuthority(email, roleName)
-        p?.delete()
     }
 }
